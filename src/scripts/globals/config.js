ab4629e705a88bfc90b85a17c1e33593672b7314
Irfando <irfandoaritonang86@gmail.com>
2022-11-15 23:44:16 +0700

    add images,config.js,edit app.js,add hero.js,etc.

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+  // BASE_URL: 'https://restaurant-api.dicoding.dev/',
+  // BASE_IMAGE_URL_SIZE_SMALL: 'https://restaurant-api.dicoding.dev/images/small/',
+  // BASE_IMAGE_URL_SIZE_MEDIUM: 'https://restaurant-api.dicoding.dev/images/medium/',
+  IMAGE_BRAND_LOGO_PATH: './images/logo-title-black',
+  IMAGE_HERO_LARGE_PATH: './images/heros/hero-large',
+  // IMAGE_HERO_SMALL_PATH: './images/heros/hero-image_2-small',
+  // IMAGE_DEFAULT_FOOD_PATH: './images/icon-food',
+  // IMAGE_DEFAULT_DRINK_PATH: './images/icon-drink',
+  // IMAGE_DEFAULT_PERSON_REVIEW_PATH: './images/icon-profile-user',
+  // DATABASE_NAME: 'restaurant-database',
+  // DATABASE_VERSION: 1,
+  // OBJECT_STORE_NAME: 'restaurant-favorite',
+};
+
+export default CONFIG;
--- src/scripts/views/components/app-bar.js
+++ src/scripts/views/components/app-bar.js
@@ -1,3 +1,5 @@
+import CONFIG from '../../globals/config';
+
 class AppBar extends HTMLElement {
   connectedCallback() {
     this._render();
@@ -10,7 +12,7 @@ class AppBar extends HTMLElement {
             <picture>
               <source type="image/webp" srcset="${CONFIG.IMAGE_BRAND_LOGO_PATH}.webp">
               <source type="image/png" srcset="${CONFIG.IMAGE_BRAND_LOGO_PATH}.png">
-              <img src="${CONFIG.IMAGE_BRAND_LOGO_PATH}.png" alt="Find Foods">
+              <img src="${CONFIG.IMAGE_BRAND_LOGO_PATH}.png" alt="Maneyfy">
             </picture>
           </a>
         </div>
@@ -21,9 +23,7 @@ class AppBar extends HTMLElement {
       
         <nav id="drawer" class="nav">
           <ul class="nav_list">
-            <li class="nav_item"><a href="#/home"><span class="material-icons" aria-hidden="true">home</span> Home</a></li>
-            <li class="nav_item"><a href="#/favorite"><span class="material-icons" aria-hidden="true">favorite</span> Favorite</a></li>
-            <li class="nav_item"><a href="https://github.com/TegarNH" target="_blank" rel="noopener"><span class="material-icons" aria-hidden="true">person</span> About Us</a></li>
+            <li class="nav_item"><a href="https://github.com/TegarNH" target="_blank" rel="noopener">Login</a></li>
           </ul>
         </nav>`;
   }
