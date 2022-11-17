8841decdab4648e039dc972556955fc46ad2381e
Irfando <irfandoaritonang86@gmail.com>
2022-11-18 00:23:30 +0700

    edit

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
   devServer: {
     static: path.resolve(__dirname, 'dist'),
     open: true,
-    port: 9001,
+    port: 8002,
     client: {
       overlay: {
         errors: true,
