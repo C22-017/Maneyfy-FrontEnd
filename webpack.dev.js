9f87cc75abe75da6c5d6775fb1b76afa2bc54349
Irfando <irfandoaritonang86@gmail.com>
2022-11-21 11:01:38 +0700

    re design landing

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
   devServer: {
     static: path.resolve(__dirname, 'dist'),
     open: true,
-    port: 8002,
+    port: 8004,
     client: {
       overlay: {
         errors: true,
