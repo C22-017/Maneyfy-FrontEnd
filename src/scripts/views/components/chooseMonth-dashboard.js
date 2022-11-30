bd0461bd5bb754d1c69f9dd3ff5b5d051c2cc23e
Irfando <irfandoaritonang86@gmail.com>
2022-11-30 23:24:07 +0700

    fix responsive for choose month

--- src/scripts/views/components/chooseMonth-dashboard.js
+++ src/scripts/views/components/chooseMonth-dashboard.js
@@ -9,16 +9,17 @@ class ChooseMonth extends HTMLElement {
       <div class="container">
         <div class="row no-gutters justify-content-center">
 
-          <div class="col-lg-4 col-md-6 d-md-flex align-items-md-stretch box-main">
+          <div class="col-lg-4 col-md-6 d-md-flex align-items-md-center box-main">
             <div class="box-month">
-              <p class="choose-title mb-3"><strong>Pilih Bulan</strong></p>
-              <input class="input-month" type="month" id="start" name="start"min="2022-11" value="">
-              
-              <span class="purecounter">
-              <button type="submit" class="btn btn-primary btn-lg button-report">
-              Tampilkan
-              </button
-              span>
+
+              <div class="main-month">
+                <p class="choose-title mb-3"><strong>Pilih Bulan</strong></p>
+                <input class="input-month" type="month" id="start" name="start"min="2022-11" value="">
+                
+                <button type="submit" class="btn btn-primary btn-lg button-report">
+                Tampilkan
+                </button>
+              </div>
 
             </div>
           </div>
