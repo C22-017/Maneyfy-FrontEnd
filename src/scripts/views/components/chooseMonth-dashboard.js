2030018f2e27c1daecc6a5187b17f476291aaf00
Irfando <irfandoaritonang86@gmail.com>
2022-11-30 15:11:43 +0700

    fix choose month but not yet responsive

--- src/scripts/views/components/chooseMonth-dashboard.js
+++ src/scripts/views/components/chooseMonth-dashboard.js
@@ -1,5 +1,3 @@
-import CONFIG from '../../globals/config';
-
 class ChooseMonth extends HTMLElement {
   connectedCallback() {
     this._render();
@@ -7,30 +5,26 @@ class ChooseMonth extends HTMLElement {
 
   _render() {
     this.innerHTML = `
-    <section>
-    <div class="container d-flex  mt-5 menu-bulan">
-      <div class="container d-flex justify-content-star pilih-bulan">
-        <h1>Pilih Bulan</h1>
+    <section class="month section-bg mt-5">
+      <div class="container">
+        <div class="row no-gutters justify-content-center">
+
+          <div class="col-lg-4 col-md-6 d-md-flex align-items-md-stretch box-main">
+            <div class="box-month">
+              <p class="choose-title mb-3"><strong>Pilih Bulan</strong></p>
+              <input class="input-month" type="month" id="start" name="start"min="2022-11" value="2022-11">
+
+              <span>
+              <button type="submit" class="btn btn-primary btn-lg button-report">
+              Tampilkan
+              </button
+              span>
+
+            </div>
+          </div>
+
+        </div>  
       </div>
-      <div>
-              <select placeholder="Pilih Bulan">
-              <option name="" value="" style="display:none;">MM</option>
-              <option name="January" value="Jan">January</option>
-              <option name="February" value="Feb">February</option>
-              <option name="March" value="Mar">March</option>
-              <option name="April" value="Apr">April</option>
-              <option name="May" value="May">May</option>
-              <option name="June" value="Jun">June</option>
-              <option name="July" value="Jul">July</option>
-              <option name="August" value="Aug">August</option>
-              <option name="September" value="Sep">September</option>
-              <option name="October" value="Oct">October</option>
-              <option name="November" value="Nov">November</option>
-              <option name="December" value="Dec">December</option>
-            </select>
-      </div>
-      
-    </div>
     </section>
   `;
   }
--- src/scripts/views/components/kategoriList-dashboard.js
+++ src/scripts/views/components/kategoriList-dashboard.js
@@ -17,7 +17,6 @@ class CategoryListDashboard extends HTMLElement {
                 <p class="category-title"><strong>Pemasukan</strong></p>
                 <img class="category-icon" src="..//public/icons/income.png" alt="">
                 <span data-purecounter-start="0" data-purecounter-end="1463" data-purecounter-duration="1" class="purecounter">+Rp. 123.000.000</span>
-                <!-- <a href="#">Find out more &raquo;</a> -->
               </div>
             </div>
   
@@ -26,7 +25,6 @@ class CategoryListDashboard extends HTMLElement {
                 <p class="category-title"><strong>Saldo Dompet</strong></p>
                 <img class="category-icon" src="..//public/icons/wallet.png" alt="">
                 <span data-purecounter-start="0" data-purecounter-end="1463" data-purecounter-duration="1" class="purecounter">Rp. 150.000</span>
-                <!-- <a href="#">Find out more &raquo;</a> -->
               </div>
             </div>
   
@@ -35,7 +33,6 @@ class CategoryListDashboard extends HTMLElement {
                 <p class="category-title"><strong>Pengeluaran</strong></p>
                 <img class="category-icon" src="..//public/icons/spend.png" alt="">
                 <span data-purecounter-start="0" data-purecounter-end="1463" data-purecounter-duration="1" class="purecounter">-Rp. 123.000.000</span>
-                <!-- <a href="#">Find out more &raquo;</a> -->
               </div>
             </div>
 
