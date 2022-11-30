964b18ef9227f2255ff1c5f75347e77648584c46
Irfando <irfandoaritonang86@gmail.com>
2022-11-30 22:11:02 +0700

    fix rsponsives for mobile

--- src/scripts/views/components/chooseMonth-dashboard.js
+++ src/scripts/views/components/chooseMonth-dashboard.js
@@ -5,16 +5,16 @@ class ChooseMonth extends HTMLElement {
 
   _render() {
     this.innerHTML = `
-    <section class="month section-bg mt-5">
+    <section class="month section-bg">
       <div class="container">
         <div class="row no-gutters justify-content-center">
 
           <div class="col-lg-4 col-md-6 d-md-flex align-items-md-stretch box-main">
             <div class="box-month">
               <p class="choose-title mb-3"><strong>Pilih Bulan</strong></p>
-              <input class="input-month" type="month" id="start" name="start"min="2022-11" value="2022-11">
-
-              <span>
+              <input class="input-month" type="month" id="start" name="start"min="2022-11" value="">
+              
+              <span class="purecounter">
               <button type="submit" class="btn btn-primary btn-lg button-report">
               Tampilkan
               </button
--- src/scripts/views/components/header-dashboard.js
+++ src/scripts/views/components/header-dashboard.js
@@ -11,10 +11,10 @@ class HeaderDashboard extends HTMLElement {
         <div class="container ">
           <div class="row">
             <div class="col-9">
-              <div class="col-4">
+              <div class="col-4 forWidth">
                 <div id="custom-search-input">
                   <div class="input-group col-md-12">
-                      <input type="text" class="form-control input-lg form-search" placeholder="Cari Transaksi" />
+                      <input type="text" class="form-control input-lg form-search searchBar" placeholder="Cari Transaksi" />
                       <span class="input-group-btn ">
                           <button class="btn btn-info" type="button">
                               <img class="icSearch" src="${CONFIG.ICON_SEARCH}" alt="">
@@ -25,9 +25,9 @@ class HeaderDashboard extends HTMLElement {
               </div>
             </div>
             <div class="col-3 d-flex justify-content-end">
-              <button type="submit" class="btn btn-primary btn-lg button-login">
-                <img class="icAdd" src="${CONFIG.ICON_ADD_TRANSACTION}" alt="">
-                Tambah Transaksi
+              <button type="submit" class="btn btn-primary btn-lg button-add">
+                <img class="icAdd" src="${CONFIG.ICON_ADD_TRANSACTION}" alt="icAdd">
+                <p class="tambah">Tambah Transaksi</p>
               </button>
             </div>
           </div>
